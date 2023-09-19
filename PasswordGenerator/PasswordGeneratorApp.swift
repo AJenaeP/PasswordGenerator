d120fd4a20a59104cb92fada9effe1f5ea0f44cf
AJenaeP <ajenae2u@yahoo.com>
2023-09-18 21:12:21 -0400

    Initial Commit

---
+++ PasswordGenerator/PasswordGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorApp.swift
+//  PasswordGenerator
+//
+//  Created by A'Jenae Pompey on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
